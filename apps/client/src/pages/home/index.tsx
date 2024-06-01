1b4020b5d4b2646d1005d8bc6f8c76e0cb6cd4d7
shoebilyas123 <shoebilyas123@gmail.com>
2024-06-01 23:31:24 +0530

    routes testing manual

--- apps/client/src/pages/home/index.tsx
+++ apps/client/src/pages/home/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1>Home</h1>
+      <div className="mt-4 flex items-center space-x-2">
+        <p
+          onClick={() => navigate('/login')}
+          className="text-sky-900 hover:text-sky-600 hover:underline active:text-sky-800"
+        >
+          Login
+        </p>
+        <p
+          onClick={() => navigate('/')}
+          className="text-sky-900 hover:text-sky-600 hover:underline active:text-sky-800"
+        >
+          Home
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
